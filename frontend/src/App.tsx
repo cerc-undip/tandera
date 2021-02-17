e0b04de74d309ec9a899dab0915da9b370a81eea
Sayyid Yofa <sayyidyofa@gmail.com>
2021-02-17 16:01:26 +0700

    Linted previous source

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import './App.css';
-import {LandingPage} from "./pages/home/Landing";
+import React from "react";
+import "./App.css";
+import { LandingPage } from "./pages/home/Landing";
 
-export const App: React.FC = () => (
-    <LandingPage/>
-);
+export const App: React.FC = () => <LandingPage />;
 
 export default App;
--- frontend/src/pages/home/Landing.tsx
+++ frontend/src/pages/home/Landing.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {Button, Container, Header, Menu, Segment} from "semantic-ui-react";
+import { Button, Container, Header, Menu, Segment } from "semantic-ui-react";
 
 export const LandingPage: React.FC = () => (
     <Segment
         inverted
-        textAlign='center'
-        style={{ minHeight: 700, padding: '1em 0em' }}
+        textAlign="center"
+        style={{ minHeight: 700, padding: "1em 0em" }}
         vertical
     >
         <Menu
@@ -13,17 +13,17 @@ export const LandingPage: React.FC = () => (
             inverted={true}
             pointing={true}
             secondary={true}
-            size='large'
+            size="large"
         >
             <Container>
-                <Menu.Item as='a' active>
+                <Menu.Item as="a" active>
                     Home
                 </Menu.Item>
-                <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
-                <Menu.Item as='a'>Careers</Menu.Item>
-                <Menu.Item position='right'>
-                    <Button as='a' inverted>
+                <Menu.Item as="a">Work</Menu.Item>
+                <Menu.Item as="a">Company</Menu.Item>
+                <Menu.Item as="a">Careers</Menu.Item>
+                <Menu.Item position="right">
+                    <Button as="a" inverted>
                         Log in
                     </Button>
                 </Menu.Item>
@@ -31,24 +31,24 @@ export const LandingPage: React.FC = () => (
         </Menu>
         <Container text>
             <Header
-                as='h1'
-                content='Tandera'
+                as="h1"
+                content="Tandera"
                 inverted
                 style={{
-                    fontSize: '4em',
-                    fontWeight: 'normal',
+                    fontSize: "4em",
+                    fontWeight: "normal",
                     marginBottom: 0,
-                    marginTop: '3em',
+                    marginTop: "3em"
                 }}
             />
             <Header
-                as='h2'
-                content='Tangkap bendera! Haha get it?'
+                as="h2"
+                content="Tangkap bendera! Haha get it?"
                 inverted
                 style={{
-                    fontSize: '1.7em',
-                    fontWeight: 'normal',
-                    marginTop: '1.5em',
+                    fontSize: "1.7em",
+                    fontWeight: "normal",
+                    marginTop: "1.5em"
                 }}
             />
         </Container>
